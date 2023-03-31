c16af2347516b0e5c0fecf4e39ca4cfee5df6756
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 00:47:59 +0530

    perimeter of rectangle

---
+++ question3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int l,b,area,perimeter;
+cout<<"enter the length of the rectangle"<<endl;
+cin>>l;
+cout<<"enter the breadth of the rectangle"<<endl;
+cin>>b;
+
+area=l*b;
+cout<<"the area of the rectangle is"<<area<<endl;
+perimeter=2*(l+b);
+cout<<"the perimeter of the rectangle is"<<perimeter;
+}
