8271e78911b0cfc8427b0a2cac642edc66bdce41
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 00:49:00 +0530

    total and average of 4 number.cpp

---
+++ question4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a,b,c,d;
+float total;
+float average;
+cout<<"input the first two number i.e a and b"<<endl;
+cin>>a>>b;
+cout<<"input the other two number i.e c and d"<<endl;
+cin>>c>>d;
+total=a+b+c+d;
+cout<<"the total of four numbers is"<<total<<endl;
+average=total/4;
+cout<<"the average of four numbers is"<<average<<endl;
+
+
+
+
+
+}
