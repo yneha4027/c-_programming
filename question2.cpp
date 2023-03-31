3fb6a70f3077885d8017ebbaf739ead4e1a2fccd
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 00:46:12 +0530

    Add files via upload

---
+++ question2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int radius;
+float volume;
+cout<<"enter the radius of the sphere"<<endl;
+cin>>radius;
+volume=(4*3.14*radius *radius)/3;
+cout<<"the volume of the sphere is"<<volume;
+
+
+
+
+
+}
