e6475377f13661f233a6cc1071dc8505f8574a46
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 01:15:37 +0530

    reverse first and second name.cpp

---
+++ reverse_name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+char first_name[30],second_name[30];
+cout<<"enter the first name"<<endl;
+cin>>first_name;
+cout<<"enter the second name"<<endl;
+cin>>second_name;
+cout<<"name after reverse is :"<<first_name<<" "<<second_name<<endl;
+
+}
