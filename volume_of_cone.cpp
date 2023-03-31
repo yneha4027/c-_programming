56bb7e6ed3e27c142b8d68f83ec7c73733655bdf
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 01:26:29 +0530

    program of volume of cone.cpp

---
+++ volume_of_cone.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+float radius,height;
+float volume;
+const float pi=3.14;
+cout<<"input the radius of cone"<<endl;
+cin>>radius;
+cout<<"input the height of cone"<<endl;
+cin>>height;
+volume=(1.0/3.0)*pi*(radius*radius)*height;
+cout<<"volume of cone is :"<<volume;
+
+
+
+
+
+
+
+}
