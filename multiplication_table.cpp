629150e5e6195969b7aa914838fac778e6ed2f18
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 01:04:26 +0530

    multiplication table.cpp

---
+++ multiplication_table.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num,i;
+cout<<"please enter the number"<<endl;
+cin>>num;
+for(i=1;i<=10;i++){
+
+    cout<<num<<"x"<<i<<"="<<num*i<<endl;
+
+}
+
+
+
+
+
+
+}
