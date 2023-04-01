1b84cd3912dd3ee1ab850e2e9faa8ce07e67ef07
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 10:52:00 +0530

    find angle of triangle.cpp

---
+++ angle_triangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int angle1,angle2,angle3;
+cout<<"Enter the first angle of the triangle:"<<endl;
+cin>>angle1;
+cout<<"enter the second angle of the triangle:"<<endl;
+cin>>angle2;
+angle3=180-(angle1+angle2);
+cout<<"The third angle of the triangle is:"<<angle3;
+
+
+
+
+
+
+
+
+}
