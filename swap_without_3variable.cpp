6329a231d21b84cfeeed9305f7bd141faeb88eaa
NEHA YADAV <91207889+yneha4027@users.noreply.github.com>
2023-04-01 10:45:20 +0530

    swap two number without using third variable.cpp

---
+++ swap_without_3variable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter the first number :"<<endl;
+    cin>>num1;
+    cout<<"enter the second number :"<<endl;
+    cin>>num2;
+    num2=num2+num1;
+    num1=num2-num1;
+    num2=num2-num1;
+    cout<<"after swapping the first number is :"<<num1<<endl;
+    cout<<"after swapping the second number is :"<<num2;
+
+
+
+
+
+
+
+
+}
